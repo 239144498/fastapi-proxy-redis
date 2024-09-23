2370f051ef1e2c632477266f3d85dbb2e8bc8b5b
Naihe <64341459+239144498@users.noreply.github.com>
2024-09-23 18:26:40 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,11 @@ import os
 from fastapi import FastAPI
 from fastapi.responses import Response, JSONResponse, HTMLResponse
 from contextlib import asynccontextmanager
-from redis import from_url
+
+if int(sys.version.split(".")[1]) > 10:
+    from redis.asyncio import from_url
+else:
+    from aioredis import from_url
 
 app = FastAPI()
 
@@ -10,7 +14,7 @@ app = FastAPI()
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     cache_redis_url = os.environ.get('REDIS_URL')
-    redis = from_url(cache_redis_url,
+    redis = await from_url(cache_redis_url,
                            encoding='utf-8',
                            decode_responses=True,
                            health_check_interval=60,
@@ -22,6 +26,7 @@ async def lifespan(app: FastAPI):
         yield
     finally:
         redis.close()
+        await redis.wait_closed()
 
 app.router.lifespan_context = lifespan
 
@@ -29,7 +34,7 @@ app.router.lifespan_context = lifespan
 @app.post("/set")
 async def set_key_value(key: str, value: str):
     try:
-        status = app.state.redis.set(key, value)
+        status = await app.state.redis.set(key, value)
         return HTMLResponse(status_code=200, content=str(status))
     except Exception as e:
         print(f"set {key} 出错 {e}")
@@ -39,7 +44,7 @@ async def set_key_value(key: str, value: str):
 @app.get("/get")
 async def get_key_value(key: str):
     try:
-        value = app.state.redis.get(key)
+        value = await app.state.redis.get(key)
         return JSONResponse(status_code=200, content={"key": key, "value": value})
     except Exception as e:
         print(f"hgetall {key} 出错 {e}")
@@ -49,7 +54,7 @@ async def get_key_value(key: str):
 @app.delete("/delete")
 async def get_key_value(key: str):
     try:
-        status = app.state.redis.delete(key)
+        status = await app.state.redis.delete(key)
         return HTMLResponse(status_code=200, content=str(status))
     except Exception as e:
         print(f"hgetall {key} 出错 {e}")
@@ -59,7 +64,7 @@ async def get_key_value(key: str):
 @app.post("/hmset")
 async def hmset_key_value(key: str, values: dict):
     try:
-        status = app.state.redis.hset(key, mapping=values)
+        status = await app.state.redis.hset(key, mapping=values)
         return HTMLResponse(status_code=200, content=str(status))
     except Exception as e:
         print(f"hmset {key} 出错 {e}")
@@ -69,7 +74,7 @@ async def hmset_key_value(key: str, values: dict):
 @app.get("/hgetall")
 async def hgetall_key(key: str):
     try:
-        values = app.state.redis.hgetall(key)
+        values = await app.state.redis.hgetall(key)
         return JSONResponse(status_code=200, content=values)
     except Exception as e:
         print(f"hgetall {key} 出错 {e}")
@@ -79,7 +84,7 @@ async def hgetall_key(key: str):
 @app.get("/hget")
 async def hget_key_value(key: str, field: str):
     try:
-        value = app.state.redis.hget(key, field)
+        value = await app.state.redis.hget(key, field)
         if value is None:
             return Response(status_code=404, content=value)
         if "chanty-data.s3.us-east-1.amazonaws.com" in value:
@@ -93,7 +98,7 @@ async def hget_key_value(key: str, field: str):
 @app.post("/hset")
 async def hset_key_value(key: str, field: str, value: str):
     try:
-        status = app.state.redis.hset(key, field, value)
+        status = await app.state.redis.hset(key, field, value)
         return HTMLResponse(status_code=200, content=str(status))
     except Exception as e:
         print(f"hset {key} 出错 {field} {e}")
@@ -103,7 +108,7 @@ async def hset_key_value(key: str, field: str, value: str):
 @app.delete("/hdel")
 async def hdel_key_field(key: str, field: str):
     try:
-        result = app.state.redis.hdel(key, field)
+        result = await app.state.redis.hdel(key, field)
         if result == 0:
             return HTMLResponse(status_code=200, content=str(result))
         return HTMLResponse(status_code=200, content=str(result))
