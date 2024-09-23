87bb76ce03e8e0f5582c18636da10e49749e0856
Naihe <64341459+239144498@users.noreply.github.com>
2024-09-23 17:27:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from app.main import app
 
 if __name__ == '__main__':
     import uvicorn
-    
     PORT = os.environ.get('PORT') or 8080
     uvicorn.run(app, host="0.0.0.0", port=PORT, log_level="info")
